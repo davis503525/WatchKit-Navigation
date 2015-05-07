11b44fa90043fbd1977be33a830779a31fcca1d8
davis503525 <davis525@live.com.au>
2015-05-07 19:27:09 +1000

    Initial Commit

---
+++ WatchKit Navigation WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WatchKit Navigation WatchKit Extension
+//
+//  Created by Davis Allie on 7/05/2015.
+//  Copyright (c) 2015 tutsplus. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
