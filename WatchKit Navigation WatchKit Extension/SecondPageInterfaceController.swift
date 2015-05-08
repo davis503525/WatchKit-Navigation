1738f278b0b7a29782d4a3bd55b4f76fb35edda1
davis503525 <davis525@live.com.au>
2015-05-08 20:28:16 +1000

    Initial Commit

---
+++ WatchKit Navigation WatchKit Extension/SecondPageInterfaceController.swift
@@ -0,0 +1,34 @@
+//
+//  SecondPageInterfaceController.swift
+//  WatchKit Navigation
+//
+//  Created by Davis Allie on 8/05/2015.
+//  Copyright (c) 2015 tutsplus. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class SecondPageInterfaceController: WKInterfaceController {
+
+    @IBOutlet var label: WKInterfaceLabel!
+    
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+        self.becomeCurrentPage()
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
